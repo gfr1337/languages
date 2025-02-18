98f89c2646385cd34abbeb40c0959a64fd33aa91
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2025-02-18 08:47:15 +0100

    Restore Fibonacci

--- fibonacci/csharp/code.cs
+++ fibonacci/csharp/code.cs
@@ -1,11 +1,5 @@
 ﻿public static class Code
 {
-    public static int Fibonacci(int n)
-    {
-        return n switch
-        {
-            <= 1 => n,
-            _ => Fibonacci(n - 1) + Fibonacci(n - 2)
-        };
-    }
+    public static int Fibonacci(int n) =>
+        n <= 1 ? n : Fibonacci(n - 1) + Fibonacci(n - 2);
 }
