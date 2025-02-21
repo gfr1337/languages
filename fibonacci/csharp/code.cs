d54daec85d6cf68bf28f00e997ee05e2f8cf378a
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2025-02-21 14:52:46 +0100

    Add Fibonacci

--- fibonacci/csharp/code.cs
+++
@@ -1,5 +0,0 @@
-﻿public static class Code
-{
-    public static int Fibonacci(int n) =>
-        n <= 1 ? n : Fibonacci(n - 1) + Fibonacci(n - 2);
-}
--- fibonacci/csharp/run.cs
+++
@@ -1,8 +0,0 @@
-var runMs = int.Parse(args[0]);
-var warmupMs = int.Parse(args[1]);
-var n = int.Parse(args[2]);
-
-Benchmark<int>.Run(() => Code.Fibonacci(n), warmupMs);
-
-var result = Benchmark<int>.Run(() => Code.Fibonacci(n), runMs);
-Console.WriteLine(result);
